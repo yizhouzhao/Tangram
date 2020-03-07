91acfb603e0fa85d1560edb9e4303a5b299a08d9
yizhouzhao <yizhou_zhao@berkeley.edu>
2020-03-06 19:02:53 -0800

    add left click rotation

--- Tangram/Assets/Scripts/ShapeMove.cs
+++ Tangram/Assets/Scripts/ShapeMove.cs
@@ -5,9 +5,21 @@ using UnityEngine;
 
 public class ShapeMove : MonoBehaviour
 {
+    //Move by left button
     private Vector3 mOffset;
-
     private float mZCoord;
+    private Vector3 currentPosition;
+
+    //Rotate by right button
+    private float rotAngle = 15f;
+    private bool canRotateR;
+    private bool canRotateL;
+
+    //Flip by double click
+    private float lastClickTime = 0f;
+    private float catchTime = 0.2f;
+    private bool canFlip;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,14 +29,43 @@ public class ShapeMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        ////Right mouse button
+        //if (Input.GetMouseButtonUp(1))
+        //{
+        //    canRotateR = false;
+        //}
+        if (canRotateR)
+        {
+            if (Input.GetMouseButtonUp(1))
+            {
+                //Rotate
+                Debug.Log("Pressed right click. rotate" + gameObject.name);
+                float rotX = rotAngle * Mathf.Deg2Rad;
+                this.gameObject.transform.Rotate(Vector3.forward, rotAngle, Space.World);
+                canRotateR = false;
+            }
+        }
+
         
+        if (canRotateL)
+        {
+            //Rotate
+            if (Input.GetMouseButtonUp(0))
+            {
+                Debug.Log("Pressed right click. rotate" + gameObject.name);
+                if (Vector3.Distance(currentPosition, this.transform.position) < 1e-2f)
+                {
+                    float rotX = rotAngle * Mathf.Deg2Rad;
+                    this.gameObject.transform.Rotate(Vector3.forward, -rotAngle, Space.World);
+                }
+                canRotateL = false;
+            }
+        }
     }
 
     private void OnMouseDown()
     {
-        //Debug.Log("ShapeMove");
-        mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
-        mOffset = gameObject.transform.position - GetMouseWorldPos();
+
     }
 
     private Vector3 GetMouseWorldPos()
@@ -39,4 +80,38 @@ public class ShapeMove : MonoBehaviour
     {
         transform.position = GetMouseWorldPos() + mOffset;
     }
+
+    private void OnMouseOver()
+    {
+        //Right mouse button
+        if (Input.GetMouseButtonDown(1))
+        {
+            canRotateR = true;
+        }
+
+        //Left mouse button
+        if (Input.GetMouseButtonDown(0))
+        {
+            canRotateL = true;
+            //Debug.Log("ShapeMove");
+            mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+            mOffset = gameObject.transform.position - GetMouseWorldPos();
+            currentPosition = this.transform.position;
+
+            if (Time.time - lastClickTime < catchTime)
+            {
+                //double click
+                print("done:" + (Time.time - lastClickTime).ToString());
+                canFlip = true;
+
+            }
+            else
+            {
+                //normal click
+                print("miss:" + (Time.time - lastClickTime).ToString());
+            }
+            lastClickTime = Time.time;
+        }
+    }
+
 }
