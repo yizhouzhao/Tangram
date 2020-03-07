9d52c8ab6f3c7b936cfb6d9214a63887b9f42fb6
yizhouzhao <yizhou_zhao@berkeley.edu>
2020-03-06 17:16:47 -0800

    script: move shape

---
+++ Tangram/Assets/Scripts/ShapeMove.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShapeMove : MonoBehaviour
+{
+    private Vector3 mOffset;
+
+    private float mZCoord;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        //Debug.Log("ShapeMove");
+        mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+        mOffset = gameObject.transform.position - GetMouseWorldPos();
+    }
+
+    private Vector3 GetMouseWorldPos()
+    {
+        Vector3 mousePoint = Input.mousePosition;
+        mousePoint.z = mZCoord;
+
+        return Camera.main.ScreenToWorldPoint(mousePoint);
+    }
+
+    private void OnMouseDrag()
+    {
+        transform.position = GetMouseWorldPos() + mOffset;
+    }
+}
